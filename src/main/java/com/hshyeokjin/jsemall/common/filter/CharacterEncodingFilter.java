6e8b29ef0eebba51e4c0069340a3c0d266356ba4
HS-hyeokjin <youngurwls@gmail.com>
2024-07-12 16:27:46 +0900

    feat : 인코딩 서블릿 구현

---
+++ src/main/java/com/hshyeokjin/jsemall/common/filter/CharacterEncodingFilter.java
@@ -0,0 +1,32 @@
+package com.hshyeokjin.jsemall.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+@WebFilter(
+        urlPatterns = {"/*"},
+        initParams = { @WebInitParam(name = "encoding", value = "UTF-8") }
+)
+public class CharacterEncodingFilter implements Filter {
+
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("encoding");
+        if(encoding == null){
+            encoding = "UTF-8";
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(encoding);
+        servletResponse.setCharacterEncoding(encoding);
+
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+}
